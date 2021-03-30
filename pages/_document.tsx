30e14cdffb81d5a19c636544326484925fd1d9e0
uihyeon.cha <uihyeon.cha@gmail.com>
2021-03-30 20:43:00 +0900

    material-ui 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,56 @@
-import Document, { DocumentContext } from 'next/document'
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import theme from '~/libs/theme'
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx)
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta
+                        name="theme-color"
+                        content={theme.palette.primary.main}
+                    />
 
-        return initialProps
+                    <link rel="icon" href="/favicon.ico" />
+
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
     }
 }
 
-export default MyDocument
+MyDocument.getInitialProps = async (ctx) => {
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () => {
+        return originalRenderPage({
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        })
+    }
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+        ...initialProps,
+        styles: [
+            ...React.Children.toArray(initialProps.styles),
+            sheets.getStyleElement(),
+        ],
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,45 @@
-import Head from 'next/head'
+import { NextPage } from 'next'
+import { Container, makeStyles, Paper, Typography } from '@material-ui/core'
 
-export default function Home() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        padding: theme.spacing(2),
+        height: '100%',
+    },
+    paper: {
+        height: '100%',
+    },
+    typography: {
+        padding: theme.spacing(1),
+    },
+}))
+
+const HomePage: NextPage = () => {
+    const classes = useStyles()
+
     return (
         <>
-            <Head>
-                <title>New Office Ceremony 2021</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-              Hello World
+            <main className={classes.root}>
+                <Container className={classes.container} maxWidth="lg">
+                    <Paper className={classes.paper} elevation={3}>
+                        <Typography
+                            className={classes.typography}
+                            component="div"
+                        >
+                            Hello World
+                        </Typography>
+                    </Paper>
+                </Container>
             </main>
         </>
     )
 }
+
+export default HomePage
